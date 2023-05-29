f657876f7e93272c6475862fd082ccc480b27d6d
pasindu-weerasingha <pasinduhas@gmail.com>
2023-05-29 23:31:02 +0530

    Initial Commit

---
+++ src/main/java/lk/ijse/dep10/student/AppInitializer.java
@@ -0,0 +1,18 @@
+package lk.ijse.dep10.student;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import java.io.IOException;
+
+public class AppInitializer extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+
+
+    }
+}
---
+++ src/main/java/lk/ijse/dep10/student/AppInitializerWrapper.java
@@ -0,0 +1,7 @@
+package lk.ijse.dep10.student;
+
+public class AppInitializerWrapper {
+    public static void main(String[] args) {
+        AppInitializer.main(args);
+    }
+}
